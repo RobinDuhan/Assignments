ef57f8e619e4f7925f4545f194464f398b7b3b33
Robin Duhan <robinduhan@gmail.com>
2020-11-17 11:35:36 +0530

    Second assignment Model creation and testing python file

---
+++ SampleTest.py
@@ -0,0 +1,10 @@
+from Model import model_compressor, model_turbo
+
+#Uncomment the below lines for testing
+# model_compressor and model_turbo takes one argument i.e path
+
+model = model_compressor("D://Data/propulsion.csv")
+#model.predict(X)
+
+#model1 = model_turbo("D://Data/propulsion.csv")
+#model1.predict(X)
