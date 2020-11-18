efdcfd270526f6e1b4ed828d8fffcd4e8af2cc30
Robin Duhan <robinduhan@gmail.com>
2020-11-18 07:07:15 +0530

    Second Commit

--- SampleTest.py
+++ SampleTest.py
@@ -1,10 +1,13 @@
 from Model import model_compressor, model_turbo
 
 #Uncomment the below lines for testing
-# model_compressor and model_turbo takes one argument i.e path
+# model_compressor and model_turbo takes one argument i.e path to train.csv
 
-model = model_compressor("D://Data/propulsion.csv")
-#model.predict(X)
+#testing_data = pd.read_csv(path)
+
+
+#model = model_compressor("D://Data/propulsion.csv")
+#model.predict(testing_data)
 
 #model1 = model_turbo("D://Data/propulsion.csv")
-#model1.predict(X)
+#model1.predict(testing_data)
